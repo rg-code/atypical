1bf9d4637bd862596f7b464e34cae9464b630e00
Rohit Gupta <rgupta6@ucmerced.edu>
2021-07-31 22:09:35 -0700

    new theme

--- pages/_document.js
+++ pages/_document.js
@@ -1,59 +1,85 @@
-import Script from 'next/script'
-import Document, {Html, Head, Main, NextScript} from 'next/document'
-import * as React from 'react'
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@material-ui/styles";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return {...initialProps}
-  }
-
   render() {
     return (
       <Html lang="en">
         <Head>
-          <Script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=UA-125864873-1"
-            strategy="afterInteractive"
-          ></Script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){window.dataLayer.push(arguments)}
-                gtag("js", new Date());
-                gtag("config", "UA-125864873-1");
-              `,
-            }}
-          ></script>
-          <link rel="preconnect" href="https://www.google-analytics.com" />
-          <link rel="shortcut icon" type="image/x-icon" href="/myAvatar.ico" />
-          <meta
-            name="description"
-            content="You can find projects Thomas Ledoux worked on and contact details on this site."
-          ></meta>
-          <meta
-            property="og:description"
-            content="You can find projects Thomas Ledoux worked on and contact details on this site."
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content="#000000" />
+          <link rel="shortcut icon" href="/img/favicon.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/img/apple-icon.png"
           />
-          <meta
-            property="og:title"
-            content="Thomas Ledoux, Frontend Developer from Ghent"
+          {/* Fonts and icons */}
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"
           />
-          <meta property="og:image" content="/me.jpeg" />
-          <meta
-            name="keywords"
-            content="HTML, CSS, JavaScript, Frontend, Ghent, Thomas, Ledoux"
-          ></meta>
+          <link
+            href="https://use.fontawesome.com/releases/v5.0.10/css/all.css"
+            rel="stylesheet"
+          />
         </Head>
         <body>
+          <div id="page-transition"></div>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+MyDocument.getInitialProps = async (ctx) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      <React.Fragment key="styles">
+        {initialProps.styles}
+        {sheets.getStyleElement()}
+      </React.Fragment>,
+    ],
+  };
+};
+
+export default MyDocument;
