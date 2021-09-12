b7995de517b0f741a2accaf6a35520965d21a2b6
Rohit Gupta <rgupta6@ucmerced.edu>
2021-09-12 13:41:04 -0700

    form test experimental

--- pages-sections/LandingPage-Sections/WorkSection.js
+++ pages-sections/LandingPage-Sections/WorkSection.js
@@ -26,52 +26,97 @@ export default function WorkSection() {
       <GridContainer justify="center">
         <GridItem cs={12} sm={12} md={8}>
           <h2 className={classes.title}>Need Help? Would like to know more?</h2>
-          <form name="contact" action="/?success=true" method="POST" data-netlify="true">
-            <GridContainer>
-              <GridItem xs={12} sm={12} md={6}>
-                <CustomInput
-                  type="hidden"
-                  name="form-name"
-                  value="contact"
-                  labelText="Your Name"
-                  id="name"
-                  formControlProps={{
-                    fullWidth: true,
-                  }}
-                />
-              </GridItem>
-              <GridItem xs={12} sm={12} md={6}>
-                <CustomInput
-                  type="hidden"
-                  name="form-name"
-                  value="contact"
-                  labelText="Your Email"
-                  id="email"
-                  formControlProps={{
-                    fullWidth: true,
-                  }}
-                />
-              </GridItem>
-              <CustomInput
-                type="hidden"
-                name="form-name"
-                value="contact"
-                labelText="Your Message"
-                id="message"
-                formControlProps={{
-                  fullWidth: true,
-                  className: classes.textArea,
-                }}
-                inputProps={{
-                  multiline: true,
-                  rows: 5,
-                }}
-              />
-              <GridItem xs={12} sm={12} md={4} className={classes.textCenter}>
-                <Button type="submit" color="warning">Send Message</Button>
-              </GridItem>
-            </GridContainer>
-          </form>
+          export default function Contact() {
+            const [success, setSuccess] = useState(false);
+
+          useEffect(() => {
+            if ( window.location.search.includes('success=true') ) {
+              setSuccess(true);
+            }
+          }, []);
+
+          return (
+            <Container maxWidth="sm">
+              <h2>CONTACT</h2>
+              {success && (
+                  <p style={{ color: "green" }}>Thanks for your message! </p>
+                )}
+              <Card>
+                <form
+                    name="contact"
+                    method="POST"
+                    action="/contact/?success=true"
+                    data-netlify="true"
+                    >
+                <input type="hidden" name="form-name" value="contact" />
+                  <TextField id="standard-basic" label="name" name="name" /> <br />
+                  <TextField id="standard-basic" label="email" name="email" /> <br />
+                  <TextField
+                    multiline
+                    id="standard-multiline-static"
+                    label="message"
+                    name="message"
+                  />
+                  <br />
+                  <div>
+                    <Button type="submit">Send</Button>
+                  </div>
+                </form>
+              </Card>
+            </Container>
+          );
+        }
+          // <form
+          //   name="contact"
+          //   action="/?success=true"
+          //   method="POST"
+          //   data-netlify="true"
+          //   >
+          //   <GridContainer>
+          //     <GridItem xs={12} sm={12} md={6}>
+          //       <CustomInput
+          //         type="hidden"
+          //         name="form-name"
+          //         value="contact"
+          //         labelText="Your Name"
+          //         id="name"
+          //         formControlProps={{
+          //           fullWidth: true,
+          //         }}
+          //       />
+          //     </GridItem>
+          //     <GridItem xs={12} sm={12} md={6}>
+          //       <CustomInput
+          //         type="hidden"
+          //         name="form-name"
+          //         value="contact"
+          //         labelText="Your Email"
+          //         id="email"
+          //         formControlProps={{
+          //           fullWidth: true,
+          //         }}
+          //       />
+          //     </GridItem>
+          //     <CustomInput
+          //       type="hidden"
+          //       name="form-name"
+          //       value="contact"
+          //       labelText="Your Message"
+          //       id="message"
+          //       formControlProps={{
+          //         fullWidth: true,
+          //         className: classes.textArea,
+          //       }}
+          //       inputProps={{
+          //         multiline: true,
+          //         rows: 5,
+          //       }}
+          //     />
+          //     <GridItem xs={12} sm={12} md={4} className={classes.textCenter}>
+          //       <Button type="submit" color="warning">Send Message</Button>
+          //     </GridItem>
+          //   </GridContainer>
+          // </form>
         </GridItem>
       </GridContainer>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,11 @@ export default class Index extends Component {
   componentDidMount = () => {
     Router.push("/landing");
   };
-
+  <form name="contact" netlify netlify-honeypot="bot-field" action='/' hidden>
+    <input type="text" name="name" />
+    <input type="email" name="email" />
+    <textarea name="message"></textarea>
+  </form>
   render() {
     return <div />;
   }
